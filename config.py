19fc03a181b3116c427853281cf2fc12f7aa1ba8
Kanghan <puttisan.kurophakornphong@gmail.com>
2024-09-12 10:06:23 +0700

    constant setting

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+Const = {
+    'C_m' : 1.0 , # membrane capacitance, in uF/cm^2
+    'g_Na' : 120.0 , # Na maximum conductances, in mS/cm^2
+    'g_K' : 36.0,  # K maximum conductances, in mS/cm^2
+    'g_L' : 0.3 , # Leak maximum conductances, in mS/cm^2
+    'E_Na' : 115.0,  # Na Nernst reversal potentials, in mV
+    'E_K' : -12.0,  # K Nernst reversal potentials, in 
+    'E_L': 10.613
+    }
